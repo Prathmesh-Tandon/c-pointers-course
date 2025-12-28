648aad39b5b70eaacac22526a7215de220cc73f8
Prathmesh-Tandon <151887347+Prathmesh-Tandon@users.noreply.github.com>
2025-12-28 10:59:25 +0000

    void

---
+++ project9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int i = 10;
+
+  void *pi = &i;
+
+  printf("pi: %p, &i: %p\n", pi, &i);
+  //printf("*pi: %d\n", *pi); //it cant be dereferenced.
+
+  int *p = NULL;
+  printf("%p, 0x%llu\n", NULL, (long long unsigned int)NULL);
+  printf("%d\n", *p);
+  return 0;
+}
